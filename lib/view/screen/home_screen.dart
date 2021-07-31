581d3e34879e078eb05d89bce080ec2d1d9c3eb9
Maxime Traverse <maxime.Traverse@orange.fr>
2021-07-31 19:02:02 +0200

    replace Elevated Button by AppButton + use AppString const values

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -1,53 +1,48 @@
-import 'package:code_source/const/Strings.dart';
+import 'package:code_source/const/AppStrings.dart';
 import 'package:code_source/utilities/get_screen_size.dart';
+import 'package:code_source/view/widget/AppButton.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double homeButtonWidth = getScreenWidth(context) * 0.9;
+
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(Strings.appTitle),
+            Text(
+              AppStrings.appTitle,
+              style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+            ),
             Column(
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    //todo : register callback
+                AppButton(
+                  label: AppStrings.signInBtnText,
+                  onPressed: () {//todo : register callback
                   },
-                  child: Text(
-                      Strings.signInBtnText
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(getScreenWidth(context) * 0.9, 35)
-                  ),
+                  width: homeButtonWidth,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    //todo : register callback
+                AppButton(
+                  label: AppStrings.logInBtnText,
+                  onPressed: () {//todo : logIn callback
                   },
-                  child: Text(
-                      Strings.logInBtnText
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: Size(getScreenWidth(context) * 0.9, 35)
-                  ),
-                ),
-
+                  width: homeButtonWidth,
+                )
               ],
             ),
             Column(
               children: [
                 Padding(
                   padding: const EdgeInsets.only(bottom: 15.0),
-                  child: Text('Nothing to show !'),
+                  child: Text(AppStrings.nothingToShow),
                 ),
                 Image.asset(
-                  ('assets/images/todolist_img.jpg'),
+                  AppStrings.toDoListImgPath,
                   height: getScreenHeight(context) * 0.5,
                 )
               ],
