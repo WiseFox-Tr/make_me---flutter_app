046b6e4a716ba4308ef4f74648e9dea3d23031c9
Maxime Traverse <maxime.Traverse@orange.fr>
2021-07-31 19:01:31 +0200

    rename appString const class

---
+++ lib/const/AppStrings.dart
@@ -0,0 +1,11 @@
+class AppStrings {
+
+  //--------------- Path -----------------
+  static const String toDoListImgPath = 'assets/images/todolist_img.jpg';
+
+  //--------------- Text -----------------
+  static const String appTitle = 'Make me';
+  static const String logInBtnText = 'Se connecter';
+  static const String nothingToShow = 'Rien à montrer !';
+  static const String signInBtnText = 'Créer un compte';
+}
--- lib/const/Strings.dart
+++
@@ -1,5 +0,0 @@
-class Strings {
-  static const String appTitle = "Make me";
-  static const String logInBtnText = "Se connecter";
-  static const String signInBtnText = "Créer un compte";
-}
