35a98b4a6465b2d2ce50df873300b308626c64b5
Maxime Traverse <maxime.Traverse@orange.fr>
2021-07-31 16:54:16 +0200

    remove useless stuff & update home property

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:code_source/view/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,21 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(),
+      home: HomeScreen(),
     );
   }
 }
