aa8d72e59614d8b889ce6eece2ee487493e9965d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-07-31 18:40:22 +0200

    add 2 utils methods to get screen width & height

---
+++ lib/utilities/get_screen_size.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/widgets.dart';
+
+double getScreenHeight(BuildContext context) => MediaQuery.of(context).size.height;
+double getScreenWidth(BuildContext context) => MediaQuery.of(context).size.width;
