f58fcaddfee3246895071d68b9ddeea01e4c7300
Maxime Traverse <maxime.Traverse@orange.fr>
2021-07-31 18:41:22 +0200

    rework of ui & add 2 buttons & temp todo img + center elements

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:code_source/const/Strings.dart';
+import 'package:code_source/utilities/get_screen_size.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,16 +8,52 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Text(Strings.appTitle),
-          TextButton(
-            onPressed: null,
-            child: Text(
-              Strings.signInBtnText
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(Strings.appTitle),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    //todo : register callback
+                  },
+                  child: Text(
+                      Strings.signInBtnText
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(getScreenWidth(context) * 0.9, 35)
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //todo : register callback
+                  },
+                  child: Text(
+                      Strings.logInBtnText
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(getScreenWidth(context) * 0.9, 35)
+                  ),
+                ),
+
+              ],
             ),
-          )
-        ],
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: Text('Nothing to show !'),
+                ),
+                Image.asset(
+                  ('assets/images/todolist_img.jpg'),
+                  height: getScreenHeight(context) * 0.5,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
