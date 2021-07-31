0893f556b496e6f519b35d9b958de201b2d5ccf7
Maxime Traverse <maxime.Traverse@orange.fr>
2021-07-31 17:44:26 +0200

    add a const class for app colors

---
+++ lib/const/AppColors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color powerBlack = Color(0xff181820);
+  static const Color lightBlack = Color(0xff21212B);
+  static const Color darkBlue = Color(0xff041955);
+  static const Color classicBlue = Color(0xff238AF8);
+  static const Color pastelBlue = Color(0xff3450A1);
+  static const Color powerPurple = Color(0xff4C1489);
+  static const Color pastelWhite = Color(0xffF4F6FD);
+}
