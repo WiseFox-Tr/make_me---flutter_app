5586042ce8f784a55a13a9c2b219cff3e25aa905
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-02 20:40:48 +0200

    add route ref & id screen

--- lib/view/app_routes.dart
+++ lib/view/app_routes.dart
@@ -1,12 +1,19 @@
-import 'package:code_source/view/screen/home_screen.dart';
+
 import 'package:flutter/material.dart';
+import 'screen/home_screen.dart';
+import 'screen/login_screen.dart';
+import 'screen/register_screen.dart';
 
 class AppRoutes {
   static Map<String, Widget Function(BuildContext)> routes = {
     IdScreen.homeScreen : (context) => HomeScreen(),
+    IdScreen.loginScreen : (context) => LoginScreen(),
+    IdScreen.registerScreen : (context) => RegisterScreen(),
   };
 }
 
 class IdScreen {
   static const homeScreen = 'home_screen';
+  static const loginScreen = 'login_screen';
+  static const registerScreen = 'register_screen';
 }
