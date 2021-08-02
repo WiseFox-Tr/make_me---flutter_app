ea3c718016a2717dbcd5eef2303cca20bcf55c85
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-02 21:04:23 +0200

    refactor inports

--- lib/view/widget/AppButton.dart
+++ lib/view/widget/AppButton.dart
@@ -1,5 +1,3 @@
-import 'package:code_source/const/AppStrings.dart';
-import 'package:code_source/utilities/get_screen_size.dart';
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:make_me/main.dart';
 
-import 'package:code_source/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
