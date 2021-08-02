68f7ad9c916cf93df3fd6e154c9341419fc0b33c
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-02 21:04:17 +0200

    add an action icon theme to change icon color + refactor inports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:code_source/const/AppColors.dart';
-import 'package:code_source/view/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:make_me/const/AppColors.dart';
+
+import 'view/app_routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light().copyWith(
         appBarTheme: AppBarTheme(
           elevation: 0.0,
-          backgroundColor: AppColors.lightWhite
+          backgroundColor: AppColors.lightWhite,
+          actionsIconTheme: IconThemeData(color: AppColors.darkBlue)
         ),
         scaffoldBackgroundColor: AppColors.lightWhite,
         elevatedButtonTheme: ElevatedButtonThemeData(
