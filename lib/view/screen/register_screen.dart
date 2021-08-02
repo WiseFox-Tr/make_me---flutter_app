cd9251d5b157dea8053ab08b89042e22ee24b3b7
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-02 21:45:59 +0200

    add a text (to reprensent logo), 3 AppTextFields & an AppButton

--- lib/view/screen/register_screen.dart
+++ lib/view/screen/register_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:make_me/const/AppColors.dart';
+import 'package:make_me/const/AppStrings.dart';
+import 'package:make_me/utilities/get_screen_size.dart';
+import 'package:make_me/view/widget/AppButton.dart';
+import 'package:make_me/view/widget/AppTextField.dart';
 import 'package:make_me/view/widget/build_app_bar.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -8,7 +13,36 @@ class RegisterScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Center(
-        child: Text("Register screen"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('Make me Logo'),
+            Column(
+              children: [
+                AppTextField(
+                  textHint: AppStrings.emailHint,
+                  onChanged: () {},
+                ),
+                AppTextField(
+                  textHint: AppStrings.passwordHint,
+                  onChanged: () {},
+                ),
+                AppTextField(
+                  textHint: AppStrings.passwordConfirmationHint,
+                  onChanged: () {},
+                ),
+
+              ],
+            ),
+            AppButton(
+              label: AppStrings.registerBtnText,
+              onPressed: () {
+                //todo : create firebase project
+                //todo : register callback
+              },
+            )
+          ],
+        )
       ),
     );
   }
