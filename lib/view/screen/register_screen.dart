bd2619d720f22507b876b12aa24086aeac5e07e6
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-02 21:07:18 +0200

    add app bar

--- lib/view/screen/register_screen.dart
+++ lib/view/screen/register_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:make_me/view/widget/build_app_bar.dart';
 
 class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(),
       body: Center(
         child: Text("Register screen"),
       ),
