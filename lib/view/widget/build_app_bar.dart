d4477a53bcd3e4628fddcef7199366f3db6bef6a
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-02 21:01:58 +0200

    create a method that build appBar

---
+++ lib/view/widget/build_app_bar.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar() {
+  return AppBar(
+    actions: [
+      IconButton(onPressed: () {}, icon: Icon(Icons.account_circle)),
+      IconButton(onPressed: () {}, icon: Icon(Icons.menu))
+    ],
+  );
+}
