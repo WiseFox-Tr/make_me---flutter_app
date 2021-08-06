194789ae8b058a3f73cdcb28783747c091c5f735
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-06 19:47:07 +0200

    add new strings values & update img path values

--- lib/const/AppStrings.dart
+++ lib/const/AppStrings.dart
@@ -1,17 +1,21 @@
 class AppStrings {
 
   //--------------- Path -----------------
-  static const String toDoListImgPath = 'assets/images/todolist_img.jpg';
+  static const String logoPath = 'assets/images/make_me_logo.svg';
+  static const String toDoListImgPath = 'assets/images/check_todo_list.svg';
 
   //--------------- Text -----------------
   // -- general content
   static const String appTitle = 'Make me';
   static const String nothingToShow = 'Rien à montrer !';
+  static const String whatHaveYouToDo = "Qu'avez vous à faire ?";
 
   // -- account management content
+  static const String askIfNoAccountYet = 'Pas encore de compte ?';
+  static const String askIfPasswordForgotten = 'Mot de passe oublié ?';
   static const String emailHint = 'e-mail';
   static const String logInBtnText = 'Se connecter';
   static const String passwordHint = 'password';
   static const String passwordConfirmationHint = 'password confirmation';
-  static const String registerBtnText = 'Créer un compte';
+  static const String registerBtnText = "S'enregistrer";
 }
