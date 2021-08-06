4f25a9e256e7035a091cc4feacdcda36ef8fd3f4
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-06 18:18:15 +0200

    replace text by AppLogo & place correctly the register button

--- lib/view/screen/register_screen.dart
+++ lib/view/screen/register_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:make_me/const/AppColors.dart';
 import 'package:make_me/const/AppStrings.dart';
 import 'package:make_me/utilities/get_screen_size.dart';
 import 'package:make_me/view/widget/AppButton.dart';
+import 'package:make_me/view/widget/AppLogo.dart';
 import 'package:make_me/view/widget/AppTextField.dart';
 import 'package:make_me/view/widget/build_app_bar.dart';
 
@@ -14,9 +16,9 @@ class RegisterScreen extends StatelessWidget {
       appBar: buildAppBar(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Make me Logo'),
+            AppLogo(),
             Column(
               children: [
                 AppTextField(
@@ -34,12 +36,21 @@ class RegisterScreen extends StatelessWidget {
 
               ],
             ),
-            AppButton(
-              label: AppStrings.registerBtnText,
-              onPressed: () {
-                //todo : create firebase project
-                //todo : register callback
-              },
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: getScreenHeight(context) * 0.03,
+                  horizontal: getScreenWidth(context) * 0.08,
+                ),
+                child: AppButton(
+                  label: AppStrings.registerBtnText,
+                  onPressed: () {
+                    //todo : create firebase project
+                    //todo : register callback
+                  },
+                ),
+              ),
             )
           ],
         )
@@ -47,3 +58,5 @@ class RegisterScreen extends StatelessWidget {
     );
   }
 }
+
+
