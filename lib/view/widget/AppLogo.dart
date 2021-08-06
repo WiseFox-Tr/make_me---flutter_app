d2a69be065cacfdc6fedbb8868f47e9f0e427fdb
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-06 18:14:14 +0200

    create logo Widget with svg picture

---
+++ lib/view/widget/AppLogo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:make_me/utilities/get_screen_size.dart';
+
+class AppLogo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: getScreenWidth(context) * 0.5,
+      height: getScreenHeight(context) * 0.2,
+      child: SvgPicture.asset("assets/images/make_me_logo.svg"),
+    );
+  }
+}
